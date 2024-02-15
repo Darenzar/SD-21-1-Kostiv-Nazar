7302deede52c1fb41542a240b6803072346c0fc9
Darenzar <nazar.o.kostiv@ukd.edu.ua>
2024-02-15 16:26:26 +0200

    3 практиична

---
+++ 3.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
+    def greet(self):
+        print("helo student")
+
+
+class Student(Person):
+
+
+a = Person("Nazar")
