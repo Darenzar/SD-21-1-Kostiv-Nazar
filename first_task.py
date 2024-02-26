71632c4835a84ced0978290daeef33f566f1a65a
JulianDemnyk <demnykjulian@gmail.com>
2024-02-26 22:05:31 +0200

    Hello my name is Julian

--- first_task.py
+++ first_task.py
@@ -1,1 +1,2 @@
 print("Hi, my name is Kostiv Nazar Olegovith and I’m studying software development in college")
+print("Hi, my name is Demnyk Julian Volodymyrovych and I’m studying software development in college")
