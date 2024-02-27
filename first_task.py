09025e9df4b20a7d3604848873f06d558239834a
Zettopraum <160030238+Zettopraum@users.noreply.github.com>
2024-02-27 23:25:12 +0200

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -1,3 +1,2 @@
 print("Hi, my name is Kostiv Nazar Olegovith and I’m studying software development in college")
 print("Hi, my name is Demnyk Julian Volodymyrovych and I’m studying software development in college")
-print("Hi, my name is Illia Myroniak Vasylovych and I’m studying software development in college")
