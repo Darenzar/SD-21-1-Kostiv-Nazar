13e80e7f51bc32bc9420681a167353dbdd175d92
Darenzar <nazar.o.kostiv@ukd.edu.ua>
2024-02-28 16:04:30 +0200

    Task completed

--- first_task.py
+++ first_task.py
@@ -1,3 +1,4 @@
 print("Hi, my name is Kostiv Nazar Olegovith and I’m studying software development in college")
 print("Hi, my name is Demnyk Julian Volodymyrovych and I’m studying software development in college")
 print("Hi, my name is Illia Myroniak Vasilyov and I’m studying software development in college")
+print("Task completed")
