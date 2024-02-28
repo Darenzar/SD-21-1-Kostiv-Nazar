6b025e441a63e4f787ef36540f5445f969bd370b
Ілля Мироняк <illia.v.myroniak@ukd.edu.ua>
2024-02-28 15:19:13 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -1,2 +1,3 @@
 print("Hi, my name is Kostiv Nazar Olegovith and I’m studying software development in college")
 print("Hi, my name is Demnyk Julian Volodymyrovych and I’m studying software development in college")
+print("Hi, my name is Illia Myroniak Vasilyov and I’m studying software development in college")
