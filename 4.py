307ef66735575e708935d30cb3ab0ba945d1a4a7
Darenzar <nazar.o.kostiv@ukd.edu.ua>
2024-02-29 16:08:56 +0200

    Add 4 task

---
+++ 4.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+c = Circle(5)
+print(c.area())
+r = Rectangle(5, 4)
+print(r.area())
